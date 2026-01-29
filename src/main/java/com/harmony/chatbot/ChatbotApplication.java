db025cc7b44aece6a6278cdfcbde015f9d5d072b
peyton150-startup <peyton150150@gmail.com>
2026-01-29 15:57:55 -0500

    Update ChatbotApplication.java

--- src/main/java/com/harmony/chatbot/ChatbotApplication.java
+++ src/main/java/com/harmony/chatbot/ChatbotApplication.java
@@ -2,12 +2,25 @@ package com.harmony.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatbotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotApplication.class, args);
+    }
 
+    // This ensures Render detects the service port
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return factory -> {
+            String port = System.getenv("PORT");  // Get Render-assigned port
+            if (port != null) {
+                factory.setPort(Integer.parseInt(port));
+            }
+        };
+    }
 }
