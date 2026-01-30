5d81b804eaa37f4b162c5747d9a87561a3b458ab
peyton150-startup <peyton150150@gmail.com>
2026-01-30 13:56:52 -0500

    Update Page.java

--- src/main/java/com/harmony/chatbot/rag/Page.java
+++ src/main/java/com/harmony/chatbot/rag/Page.java
@@ -1,5 +1,8 @@
 package com.harmony.chatbot.rag;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Page {
     private String id;
     private String slug;
