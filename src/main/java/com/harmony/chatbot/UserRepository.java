24700da8c385c7e6bbdf3696063e53a06b67794c
peyton150-startup <peyton150150@gmail.com>
2026-02-01 14:52:55 -0500

    Update UserRepository.java

--- src/main/java/com/harmony/chatbot/UserRepository.java
+++ src/main/java/com/harmony/chatbot/UserRepository.java
@@ -1,1 +1,8 @@
+package com.harmony.chatbot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    // you can add custom queries here later
+}
 
