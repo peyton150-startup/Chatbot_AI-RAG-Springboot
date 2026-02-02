a2ffbcabc9361dcc4510f53ef662a1740fc1efef
peyton150-startup <peyton150150@gmail.com>
2026-02-02 17:48:34 -0500

    Create ViewController.java

---
+++ src/main/java/com/harmony/chatbot/admin/ViewController.java
@@ -0,0 +1,18 @@
+package com.harmony.chatbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // renders login.html
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin"; // renders admin.html
+    }
+}
