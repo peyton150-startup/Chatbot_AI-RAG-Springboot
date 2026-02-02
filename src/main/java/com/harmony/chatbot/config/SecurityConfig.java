e76286c36d8f98165cdb1500b3faa37ce05523db
peyton150-startup <peyton150150@gmail.com>
2026-02-02 15:27:07 -0500

    Update SecurityConfig.java

--- src/main/java/com/harmony/chatbot/config/SecurityConfig.java
+++ src/main/java/com/harmony/chatbot/config/SecurityConfig.java
@@ -1,1 +1,16 @@
+package com.harmony.chatbot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
 
