f2c83cec282ef18604f97943601d9c5d5d4058f8
peyton150-startup <peyton150150@gmail.com>
2026-02-01 19:27:31 -0500

    Update UserService.java

--- src/main/java/com/harmony/chatbot/user/UserService.java
+++ src/main/java/com/harmony/chatbot/user/UserService.java
@@ -1,1 +1,52 @@
+package com.harmony.chatbot.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Get all users
+     */
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Get a user by ID
+     */
+    public Optional<UserEntity> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    /**
+     * Create or update a user
+     */
+    public UserEntity saveUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    /**
+     * Delete a user by ID
+     */
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    /**
+     * Check if a user exists
+     */
+    public boolean userExists(Long id) {
+        return userRepository.existsById(id);
+    }
+}
 
