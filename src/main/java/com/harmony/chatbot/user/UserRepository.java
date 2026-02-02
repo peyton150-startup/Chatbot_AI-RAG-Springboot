9830a9173d7ba504c034ddc5c7a6769099d9585d
peyton150-startup <peyton150150@gmail.com>
2026-02-02 15:48:54 -0500

    Update UserRepository.java

--- src/main/java/com/harmony/chatbot/user/UserRepository.java
+++ src/main/java/com/harmony/chatbot/user/UserRepository.java
@@ -1,7 +1,11 @@
 package com.harmony.chatbot.user;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    // you can add custom queries here later
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
-
