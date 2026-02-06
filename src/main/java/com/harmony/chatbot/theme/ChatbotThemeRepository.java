913b1f2bb90443add010d1c83111926854e34522
peyton150-startup <peyton150150@gmail.com>
2026-02-06 13:44:06 -0500

    Update ChatbotThemeRepository.java

--- src/main/java/com/harmony/chatbot/theme/ChatbotThemeRepository.java
+++ src/main/java/com/harmony/chatbot/theme/ChatbotThemeRepository.java
@@ -1,7 +1,11 @@
 package com.harmony.chatbot.theme;
 
+import com.harmony.chatbot.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatbotThemeRepository
-        extends JpaRepository<ChatbotThemeEntity, Long> {
+import java.util.Optional;
+
+public interface ChatbotThemeRepository extends JpaRepository<Theme, Long> {
+
+    Optional<Theme> findByUser(UserEntity user);
 }
