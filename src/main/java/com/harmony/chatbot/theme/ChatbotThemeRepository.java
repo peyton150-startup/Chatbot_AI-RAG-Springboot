ae8485070f0ff43058fb7cbcee2a755c8452a3d5
peyton150-startup <peyton150150@gmail.com>
2026-02-06 12:25:50 -0500

    Create ChatbotThemeRepository.java

---
+++ src/main/java/com/harmony/chatbot/theme/ChatbotThemeRepository.java
@@ -0,0 +1,7 @@
+package com.harmony.chatbot.theme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatbotThemeRepository
+        extends JpaRepository<ChatbotThemeEntity, Long> {
+}
