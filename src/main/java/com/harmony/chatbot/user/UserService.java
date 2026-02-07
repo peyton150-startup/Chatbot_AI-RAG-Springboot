eb39c8b7dd8ebd2d6811aed9dc0deea2b23e8f2f
peyton150-startup <peyton150150@gmail.com>
2026-02-07 13:37:13 -0500

    Update UserService.java

--- src/main/java/com/harmony/chatbot/user/UserService.java
+++ src/main/java/com/harmony/chatbot/user/UserService.java
@@ -43,14 +43,15 @@ public class UserService implements UserDetailsService {
     }
 
     public UserEntity saveUser(UserEntity user) {
-        if (user.getPassword() != null && !user.getPassword().startsWith("$2")) {
+        if (user.getPassword() != null &&
+            !user.getPassword().startsWith("$2")) { // encode if not already encoded
             user.setPassword(passwordEncoder.encode(user.getPassword()));
         }
 
         UserEntity saved = userRepository.save(user);
 
-        // Publish event AFTER saving user to create theme
-        eventPublisher.publishEvent(new UserCreatedEvent(saved.getId()));
+        // Publish event AFTER saving user
+        eventPublisher.publishEvent(UserCreatedEvent.of(this, saved.getId()));
 
         return saved;
     }
