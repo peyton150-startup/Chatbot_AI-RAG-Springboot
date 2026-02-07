1af27adffd83fea10a3e0c35cbc85562d50138c5
peyton150-startup <peyton150150@gmail.com>
2026-02-07 13:26:45 -0500

    Create ChatbotThemeController.java

---
+++ src/main/java/com/harmony/chatbot/theme/ChatbotThemeController.java
@@ -0,0 +1,19 @@
+package com.harmony.chatbot.theme;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatbotThemeController {
+
+    private final ChatbotThemeService themeService;
+
+    public ChatbotThemeController(ChatbotThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @GetMapping("/api/theme")
+    public ChatbotThemeEntity getCurrentUserTheme() {
+        return themeService.getThemeForCurrentUser();
+    }
+}
