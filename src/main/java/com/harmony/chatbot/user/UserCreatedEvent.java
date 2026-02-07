f15e495c85c38bd54af141ed82a845bdf085b92a
peyton150-startup <peyton150150@gmail.com>
2026-02-07 12:31:32 -0500

    Create UserCreatedEvent.java

---
+++ src/main/java/com/harmony/chatbot/user/UserCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.harmony.chatbot.user;
+
+public class UserCreatedEvent {
+
+    private final Long userId;
+
+    public UserCreatedEvent(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
