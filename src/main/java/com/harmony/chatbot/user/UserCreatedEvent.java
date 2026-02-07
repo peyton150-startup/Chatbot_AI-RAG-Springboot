091427cbc7d562882db10555c662c923d9c37f01
peyton150-startup <peyton150150@gmail.com>
2026-02-07 13:22:42 -0500

    Update UserCreatedEvent.java

--- src/main/java/com/harmony/chatbot/user/UserCreatedEvent.java
+++ src/main/java/com/harmony/chatbot/user/UserCreatedEvent.java
@@ -1,14 +1,24 @@
 package com.harmony.chatbot.user;
 
-public class UserCreatedEvent {
+import org.springframework.context.ApplicationEvent;
+
+public class UserCreatedEvent extends ApplicationEvent {
 
     private final Long userId;
 
-    public UserCreatedEvent(Long userId) {
+    public UserCreatedEvent(Object source, Long userId) {
+        super(source); // Spring requires a source object
         this.userId = userId;
     }
 
     public Long getUserId() {
         return userId;
+    }
+
+    /**
+     * Convenience factory method
+     */
+    public static UserCreatedEvent of(Object source, Long userId) {
+        return new UserCreatedEvent(source, userId);
     }
 }
